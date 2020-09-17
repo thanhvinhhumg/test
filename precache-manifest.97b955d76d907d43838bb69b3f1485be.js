225f2fb8984f7b5d02f92d5a53b91f720f610245
thanhvinhhumg <thanhvinh200293@gmail.com>
2020-09-17 16:58:55 +0700

    Updates

---
+++ precache-manifest.97b955d76d907d43838bb69b3f1485be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352ae32dfda8cf2cd739238735efb841",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "1dbab4a20380451e1d8f",
+    "url": "/test/static/css/main.5d87f937.chunk.css"
+  },
+  {
+    "revision": "453331637014d17934ff",
+    "url": "/test/static/js/2.f2eb576e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.f2eb576e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dbab4a20380451e1d8f",
+    "url": "/test/static/js/main.51fe9567.chunk.js"
+  },
+  {
+    "revision": "c1802483565493cb939a",
+    "url": "/test/static/js/runtime-main.9b24d631.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
